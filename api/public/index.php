968b1b9f5de7fd98fcb928bf6e529268f0d389a6
Igor Moiseev <developer.modx@gmail.com>
2020-09-20 14:58:15 +0300

    Add DI config

--- api/public/index.php
+++ api/public/index.php
@@ -2,13 +2,17 @@
 
 declare(strict_types=1);
 
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+/** @var ContainerInterface $container */
+$container = require __DIR__ . '/../config/container.php';
+
+$app = AppFactory::createFromContainer($container);
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write('{"message": "Hello, TodoApp!"}');

3d562309e5b771d20a7f994abf0e76f317679fda
Igor Moiseev <developer.modx@gmail.com>
2020-10-11 01:20:47 +0300

    Implemented PasswordHasher

--- api/src/Auth/Service/PasswordHasher.php
+++ api/src/Auth/Service/PasswordHasher.php
@@ -4,8 +4,37 @@ declare(strict_types=1);
 
 namespace App\Auth\Service;
 
-interface PasswordHasher
+use RuntimeException;
+use Webmozart\Assert\Assert;
+
+class PasswordHasher
 {
-    public function hash(string $password): string;
-    public function validate(string $password, string $hash): bool;
+    private int $memoryCost;
+
+    public function __construct(int $memoryCost = PASSWORD_ARGON2_DEFAULT_MEMORY_COST)
+    {
+        $this->memoryCost = $memoryCost;
+    }
+
+    public function hash(string $password): string
+    {
+        Assert::notEmpty($password);
+        /** @var string|false|null $hash */
+        $hash = password_hash($password, PASSWORD_ARGON2I, ['memory_cost' => $this->memoryCost]);
+
+        if ($hash === null) {
+            throw new RuntimeException('Invalid hash algorithm');
+        }
+
+        if ($hash === false) {
+            throw new RuntimeException('Unable to generate hash');
+        }
+
+        return $hash;
+    }
+
+    public function validate(string $password, string $hash): bool
+    {
+        return password_verify($password, $hash);
+    }
 }

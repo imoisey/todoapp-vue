6025ef72ddbe8708c8c0a7976d2e7c488b236797
Igor Moiseev <developer.modx@gmail.com>
2020-10-16 07:53:26 +0300

    Added ChangingEmail confirm use case

---
+++ api/src/Auth/Test/Unit/Entity/User/User/ChangeEmail/ConfirmTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Test\Unit\Entity\User\User\ChangeEmail;
+
+use App\Auth\Entity\User\Email;
+use App\Auth\Entity\User\Token;
+use App\Auth\Test\Builder\UserBuilder;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+class ConfirmTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $user = (new UserBuilder())->active()->build();
+
+        $now = new DateTimeImmutable();
+        $token = new Token(Uuid::uuid4()->toString(), $now->modify('+1 day'));
+
+        $user->requestEmailChanging($token, $now, $new = new Email('new@email.ru'));
+
+        self::assertNotNull($user->getNewEmailToken());
+
+        $user->confirmEmailChanging($token->getValue(), $now);
+
+        self::assertNull($user->getNewEmail());
+        self::assertNull($user->getNewEmailToken());
+        self::assertEquals($new, $user->getEmail());
+    }
+
+    public function testInvalidToken(): void
+    {
+        $user = (new UserBuilder())->active()->build();
+
+        $now = new DateTimeImmutable();
+        $token = new Token(Uuid::uuid4()->toString(), $now->modify('+1 day'));
+
+        $user->requestEmailChanging($token, $now, $new = new Email('new@email.ru'));
+
+        self::assertNotNull($user->getNewEmailToken());
+
+        $this->expectExceptionMessage('Token is invalid.');
+        $user->confirmEmailChanging('invalid-token', $now);
+    }
+
+    public function testExpiredToken(): void
+    {
+        $user = (new UserBuilder())->active()->build();
+
+        $now = new DateTimeImmutable();
+        $token = new Token(Uuid::uuid4()->toString(), $now->modify('+1 day'));
+
+        $user->requestEmailChanging($token, $now, new Email('new@email.ru'));
+
+        $this->expectExceptionMessage('Token is expired.');
+        $user->confirmEmailChanging($token->getValue(), $now->modify('+1 day'));
+    }
+
+    public function testNotRequired(): void
+    {
+        $user = (new UserBuilder())->active()->build();
+
+        $now = new DateTimeImmutable();
+        $token = new Token(Uuid::uuid4()->toString(), $now->modify('+1 day'));
+
+        $this->expectExceptionMessage('Changing is not requested.');
+        $user->confirmEmailChanging($token->getValue(), $now->modify('+1 day'));
+    }
+}

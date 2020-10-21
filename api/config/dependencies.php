15170a4c96fde94795861515554dad1ad858377d
Igor Moiseev <developer.modx@gmail.com>
2020-10-21 21:06:18 +0300

    Refactored config loader

--- api/config/dependencies.php
+++ api/config/dependencies.php
@@ -2,21 +2,12 @@
 
 declare(strict_types=1);
 
-$files = array_merge(
-    glob(__DIR__ . '/common/*.php') ?: [],
-    glob(__DIR__ . '/' . (getenv('APP_ENV') ?: 'production') . '/*.php') ?: []
-);
+use Laminas\ConfigAggregator\ConfigAggregator;
+use Laminas\ConfigAggregator\PhpFileProvider;
 
-$configs = array_map(
-    static function ($file): array {
-        /**
-         * @var array
-         * @noinspection PhpIncludeInspection
-         * @psalm-suppress UnresolvableInclude
-         */
-        return require $file;
-    },
-    $files
-);
+$aggregator = new ConfigAggregator([
+    new PhpFileProvider(__DIR__ . '/common/*.php'),
+    new PhpFileProvider(__DIR__ . '/' . (getenv('APP_ENV') ?: 'production') . '/*.php'),
+]);
 
-return array_replace_recursive(...$configs);
+return $aggregator->getMergedConfig();

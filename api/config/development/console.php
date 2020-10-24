54fd6b5e927520a44db528cc2bde59320439bc7c
Igor Moiseev <developer.modx@gmail.com>
2020-10-25 01:18:32 +0300

    Added FixturesLoadCommand

--- api/config/development/console.php
+++ api/config/development/console.php
@@ -2,14 +2,37 @@
 
 declare(strict_types=1);
 
+use App\Console\Command\FixturesLoadCommand;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Console\Command\SchemaTool;
+use Psr\Container\ContainerInterface;
 
 return [
-  'config' => [
-      'console' => [
-          'commands' => [
-              SchemaTool\DropCommand::class
-          ]
-      ]
-  ]
+    FixturesLoadCommand::class => static function (ContainerInterface $container) {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @psalm-var array{fixture_paths:string[]} $config
+         */
+        $config = $container->get('config')['console'];
+
+        /** @var EntityManagerInterface $em */
+        $em = $container->get(EntityManagerInterface::class);
+
+        return new FixturesLoadCommand(
+            $em,
+            $config['fixture_paths'],
+        );
+    },
+
+    'config' => [
+        'console' => [
+            'commands' => [
+                FixturesLoadCommand::class,
+                SchemaTool\DropCommand::class
+            ],
+            'fixture_paths' => [
+                __DIR__ . '/../../src/Auth/Fixture',
+            ],
+        ],
+    ],
 ];

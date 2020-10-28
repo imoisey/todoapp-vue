ee8a53adfe01e5f2736521aeb2903cf07e932cc0
Igor Moiseev <developer.modx@gmail.com>
2020-10-28 23:04:46 +0300

    Added twig for JoinConfirmSender

--- api/src/Auth/Service/JoinConfirmSender.php
+++ api/src/Auth/Service/JoinConfirmSender.php
@@ -10,25 +10,28 @@ use App\Frontend\UrlGenerator;
 use RuntimeException;
 use Swift_Mailer;
 use Swift_Message;
+use Twig\Environment;
 
 class JoinConfirmSender
 {
     private Swift_Mailer $mailer;
     private UrlGenerator $urlGenerator;
+    private Environment $twig;
 
-    public function __construct(Swift_Mailer $mailer, UrlGenerator $urlGenerator)
+    public function __construct(Swift_Mailer $mailer, UrlGenerator $urlGenerator, Environment $twig)
     {
         $this->mailer = $mailer;
         $this->urlGenerator = $urlGenerator;
+        $this->twig = $twig;
     }
 
     public function send(Email $email, Token $token): void
     {
         $message = (new Swift_Message('Join Confirmation'))
             ->setTo($email->getValue())
-            ->setBody($this->urlGenerator->generate('join/confirm', [
-                'token' => $token->getValue(),
-            ]));
+            ->setBody($this->twig->render('auth/join/confirm.html.twig', [
+                'url' => $this->urlGenerator->generate('join/confirm', ['token' => $token->getValue()]),
+            ]), 'text/html');
 
         if ($this->mailer->send($message) === 0) {
             throw new RuntimeException('Unable to send email.');
--- api/src/Auth/Test/Unit/Service/JoinConfirmSenderTest.php
+++ api/src/Auth/Test/Unit/Service/JoinConfirmSenderTest.php
@@ -14,6 +14,7 @@ use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\Uuid;
 use Swift_Mailer;
 use Swift_Message;
+use Twig\Environment;
 
 /**
  * @covers JoinConfirmSender
@@ -32,16 +33,23 @@ class JoinConfirmSenderTest extends TestCase
             self::equalTo(['token' => $token->getValue()]),
         )->willReturn($confirmUrl);
 
+        $twig = $this->createMock(Environment::class);
+        $twig->expects(self::once())->method('render')->with(
+            self::equalTo('auth/join/confirm.html.twig'),
+            self::equalTo(['url' => $confirmUrl]),
+        )->willReturn($body = '<a href="' . $confirmUrl . '">' . $confirmUrl . '</a>');
+
         $mailer = $this->createMock(Swift_Mailer::class);
         $mailer->expects(self::once())->method('send')
-            ->willReturnCallback(static function (Swift_Message $message) use ($to, $confirmUrl): int {
+            ->willReturnCallback(static function (Swift_Message $message) use ($to, $body): int {
                 self::assertEquals([$to->getValue() => null], $message->getTo());
                 self::assertEquals('Join Confirmation', $message->getSubject());
-                self::assertStringContainsString($confirmUrl, $message->getBody());
+                self::assertEquals($body, $message->getBody());
+                self::assertEquals('text/html', $message->getBodyContentType());
                 return 1;
             });
 
-        $sender = new JoinConfirmSender($mailer, $urlGenerator);
+        $sender = new JoinConfirmSender($mailer, $urlGenerator, $twig);
 
         $sender->send($to, $token);
     }
@@ -50,16 +58,20 @@ class JoinConfirmSenderTest extends TestCase
     {
         $to = new Email('user@app.test');
         $token = new Token(Uuid::uuid4()->toString(), new DateTimeImmutable());
+        $confirmUrl = 'http://test/join/confirm?token=' . $token->getValue();
 
         $urlGenerator = $this->createStub(UrlGenerator::class);
         $urlGenerator
             ->method('generate')
             ->willReturn('http://test/join/confirm?token=' . $token->getValue());
 
+        $twig = $this->createStub(Environment::class);
+        $twig->method('render')->willReturn('<a href="' . $confirmUrl . '">' . $confirmUrl . '</a>');
+
         $mailer = $this->createStub(Swift_Mailer::class);
         $mailer->method('send')->willReturn(0);
 
-        $sender = new JoinConfirmSender($mailer, $urlGenerator);
+        $sender = new JoinConfirmSender($mailer, $urlGenerator, $twig);
 
         $this->expectException(RuntimeException::class);
         $sender->send($to, $token);

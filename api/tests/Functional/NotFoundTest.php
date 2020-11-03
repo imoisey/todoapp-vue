5ba66a7b3d8487b80abd0481d352cf381322ae58
Igor Moiseev <developer.modx@gmail.com>
2020-11-03 07:30:11 +0300

    Upgrade NotFoundTest

--- api/tests/Functional/NotFoundTest.php
+++ api/tests/Functional/NotFoundTest.php
@@ -2,13 +2,24 @@
 
 namespace Test\Functional;
 
+use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
+
 class NotFoundTest extends WebTestCase
 {
+    use ArraySubsetAsserts;
+
     public function testNotFound(): void
     {
         $response = $this->app()->handle(self::json('GET', '/page-not-found'));
 
         self::assertEquals(404, $response->getStatusCode());
         self::assertJson($body = (string)$response->getBody());
+
+        /** @var array $data */
+        $data = Json::decode($body);
+
+        self::assertArraySubset([
+            'message' => '404 Not Found',
+        ], $data);
     }
 }

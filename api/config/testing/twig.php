d423a29a1d13c7bfb3368dad4354e205866814a2
Igor Moiseev <developer.modx@gmail.com>
2020-11-04 20:38:56 +0300

    Disabled cache of twig for testing

--- api/config/testing/twig.php
+++ api/config/testing/twig.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     'config' => [
         'twig' => [
-            'cache_dir' => __DIR__ . '/../../var/cache/' . PHP_SAPI . '/twig',
+            'cache_dir' => false,
         ],
     ],
 ];

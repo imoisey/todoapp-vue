8022f9ab5c9a4d521d34a5776e680bfc3dbb6030
Igor Moiseev <developer.modx@gmail.com>
2020-11-04 20:41:46 +0300

    Fixed error to auth/join/request handler

--- api/src/Auth/Command/JoinByEmail/Request/Handler.php
+++ api/src/Auth/Command/JoinByEmail/Request/Handler.php
@@ -42,7 +42,7 @@ class Handler
         $email = new Email($command->email);
 
         if ($this->users->hasByEmail($email)) {
-            throw new DomainException('User already exists');
+            throw new DomainException('User already exists.');
         }
 
         $date = new DateTimeImmutable();

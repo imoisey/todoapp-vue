11ebb063f8e18dde4fc9a61310104d32ceee6cac
Igor Moiseev <developer.modx@gmail.com>
2020-11-04 21:56:03 +0300

    Added loadFixtures for tests

--- api/tests/Functional/WebTestCase.php
+++ api/tests/Functional/WebTestCase.php
@@ -2,6 +2,11 @@
 
 namespace Test\Functional;
 
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -34,6 +39,25 @@ class WebTestCase extends TestCase
         }
 
         return $this->app;
+    }
+
+    /**
+     * @param array<string|int,string> $fixtures
+     */
+    protected function loadFixtures(array $fixtures): void
+    {
+        /** @var ContainerInterface $container */
+        $container = $this->app()->getContainer();
+        $loader = new Loader();
+        foreach ($fixtures as $name => $class) {
+            /** @var AbstractFixture $fixture */
+            $fixture = $container->get($class);
+            $loader->addFixture($fixture);
+        }
+        /** @var EntityManagerInterface $em */
+        $em = $container->get(EntityManagerInterface::class);
+        $executor = new ORMExecutor($em, new ORMPurger($em));
+        $executor->execute($loader->getFixtures());
     }
 
     private function container(): ContainerInterface

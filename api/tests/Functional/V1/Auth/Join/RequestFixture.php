bdf6a1a09c6fb07743e60efefbd21ff80f8e272b
Igor Moiseev <developer.modx@gmail.com>
2020-11-04 21:58:08 +0300

    Added RequestFixture for tests

---
+++ api/tests/Functional/V1/Auth/Join/RequestFixture.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional\V1\Auth\Join;
+
+use App\Auth\Entity\User\Email;
+use App\Auth\Entity\User\Id;
+use App\Auth\Entity\User\Token;
+use App\Auth\Entity\User\User;
+use DateTimeImmutable;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Persistence\ObjectManager;
+use Ramsey\Uuid\Uuid;
+
+class RequestFixture extends AbstractFixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $user = User::joinByEmail(
+            Id::generate(),
+            $date = new DateTimeImmutable('-30 days'),
+            new Email('existing@email.ru'),
+            'password-hash',
+            new Token($value = Uuid::uuid4()->toString(), $date->modify('+1 day'))
+        );
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}

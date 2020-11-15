9c4afb583fe1b6d3f16583bdbfd7e25d09b48976
Igor Moiseev <developer.modx@gmail.com>
2020-11-15 22:16:45 +0300

    Added validation translations

---
+++ api/config/common/translator.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Translation\Loader\PhpFileLoader;
+use Symfony\Component\Translation\Loader\XliffFileLoader;
+use Symfony\Component\Translation\Translator;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+return [
+    TranslatorInterface::class => static function (ContainerInterface $container): TranslatorInterface {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @psalm-var array{lang:string,resources:array<string[]>} $config
+         */
+        $config = $container->get('config')['translator'];
+
+        $translator = new Translator($config['lang']);
+        $translator->addLoader('php', new PhpFileLoader());
+        $translator->addLoader('xlf', new XliffFileLoader());
+
+        foreach ($config['resources'] as $resource) {
+            $translator->addResource(...$resource);
+        }
+
+        return $translator;
+    },
+
+    'config' => [
+        'translator' => [
+            'lang' => 'en',
+            'resources' => [
+                [
+                    'xlf',
+                    __DIR__ . '/../../vendor/symfony/validator/Resources/translations/validators.ru.xlf',
+                    'ru',
+                    'validators'
+                ],
+            ],
+        ],
+    ],
+];

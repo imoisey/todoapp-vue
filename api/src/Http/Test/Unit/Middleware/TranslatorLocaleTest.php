d1b363b71c987565353e08761a52d1d38ee204d4
Igor Moiseev <developer.modx@gmail.com>
2020-11-17 07:39:46 +0300

    Refactored TranslatorLocale

--- api/src/Http/Test/Unit/Middleware/TranslatorLocaleTest.php
+++ api/src/Http/Test/Unit/Middleware/TranslatorLocaleTest.php
@@ -18,11 +18,9 @@ class TranslatorLocaleTest extends TestCase
     public function testDefault(): void
     {
         $translator = $this->createMock(Translator::class);
-        $translator->expects(self::once())->method('setLocale')->with(
-            self::equalTo('en')
-        );
+        $translator->expects(self::never())->method('setLocale');
 
-        $middleware = new TranslatorLocale($translator, ['en', 'ru']);
+        $middleware = new TranslatorLocale($translator);
 
         $handler = $this->createStub(RequestHandlerInterface::class);
         $handler->method('handle')->willReturn($source = self::createResponse());
@@ -39,46 +37,12 @@ class TranslatorLocaleTest extends TestCase
             self::equalTo('ru')
         );
 
-        $middleware = new TranslatorLocale($translator, ['en', 'ru']);
+        $middleware = new TranslatorLocale($translator);
 
         $handler = $this->createStub(RequestHandlerInterface::class);
         $handler->method('handle')->willReturn(self::createResponse());
 
         $request = self::createRequest()->withHeader('Accept-Language', 'ru');
-
-        $middleware->process($request, $handler);
-    }
-
-    public function testMulti(): void
-    {
-        $translator = $this->createMock(Translator::class);
-        $translator->expects(self::once())->method('setLocale')->with(
-            self::equalTo('ru')
-        );
-
-        $middleware = new TranslatorLocale($translator, ['en', 'fr', 'ru']);
-
-        $handler = $this->createStub(RequestHandlerInterface::class);
-        $handler->method('handle')->willReturn(self::createResponse());
-
-        $request = self::createRequest()->withHeader('Accept-Language', 'es;q=0.9, ru;q=0.8, *;q=0.5');
-
-        $middleware->process($request, $handler);
-    }
-
-    public function testOther(): void
-    {
-        $translator = $this->createMock(Translator::class);
-        $translator->expects(self::once())->method('setLocale')->with(
-            self::equalTo('en')
-        );
-
-        $middleware = new TranslatorLocale($translator, ['en', 'ru']);
-
-        $handler = $this->createStub(RequestHandlerInterface::class);
-        $handler->method('handle')->willReturn(self::createResponse());
-
-        $request = self::createRequest()->withHeader('Accept-Language', 'es');
 
         $middleware->process($request, $handler);
     }

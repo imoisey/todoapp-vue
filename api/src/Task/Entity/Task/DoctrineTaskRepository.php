45085d4de6ad62e2cbd07d232f9927faddb7a75a
Igor Moiseev <developer.modx@gmail.com>
2020-12-04 23:24:51 +0300

    Added DoctrineTaskRepository

---
+++ api/src/Task/Entity/Task/DoctrineTaskRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Task\Entity\Task;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use DomainException;
+
+class DoctrineTaskRepository implements TaskRepository
+{
+    private EntityManagerInterface $em;
+    private EntityRepository $repository;
+
+    public function __construct(EntityManagerInterface $em, EntityRepository $repository)
+    {
+        $this->em = $em;
+        $this->repository = $repository;
+    }
+
+    public function get(Id $id): Task
+    {
+        if (!$task = $this->repository->find($id->getValue())) {
+            throw new DomainException('Task is not found.');
+        }
+
+        /** @var Task $task */
+        return $task;
+    }
+
+    public function add(Task $task): void
+    {
+        $this->em->persist($task);
+    }
+
+    public function remove(Task $task): void
+    {
+        $this->em->remove($task);
+    }
+}

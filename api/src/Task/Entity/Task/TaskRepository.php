6a67fe8be7d17bf0fdf2ba04384254ec2358459e
Igor Moiseev <developer.modx@gmail.com>
2020-12-04 23:23:49 +0300

    Added get method for TaskRepository

--- api/src/Task/Entity/Task/TaskRepository.php
+++ api/src/Task/Entity/Task/TaskRepository.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace App\Task\Entity\Task;
 
+use DomainException;
+
 interface TaskRepository
 {
+    /**
+     * @param Id $id
+     * @return Task
+     * @throws DomainException;
+     */
+    public function get(Id $id): Task;
+
     public function add(Task $task): void;
     public function remove(Task $task): void;
 }

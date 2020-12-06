2c621dbc23823ecc94090291337ecdd7906b609f
Igor Moiseev <developer.modx@gmail.com>
2020-12-06 22:58:55 +0300

    Marked skipped tests

--- api/tests/Functional/V1/Task/CreateTest.php
+++ api/tests/Functional/V1/Task/CreateTest.php
@@ -19,6 +19,8 @@ class CreateTest extends WebTestCase
 
     public function testMethod(): void
     {
+        self::markTestIncomplete('Not release.');
+
         $response = $this->app()->handle(self::json('GET', '/v1/task/create'));
 
         self::assertEquals(405, $response->getStatusCode());
@@ -26,6 +28,8 @@ class CreateTest extends WebTestCase
 
     public function testSuccess(): void
     {
+        self::markTestIncomplete('Not release.');
+
         $response = $this->app()->handle(self::json('POST', '/v1/task/create', [
             'author' => CreateFixture::USER_UUID,
             'name' => 'New Task',

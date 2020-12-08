8d67cd9db3539cb736eb1156effc1da50f073b54
Igor Moiseev <developer.modx@gmail.com>
2020-12-08 07:28:15 +0300

    Fixed tests for Task\Create

--- api/src/Task/Command/Create/Command.php
+++ api/src/Task/Command/Create/Command.php
@@ -9,6 +9,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 class Command
 {
     /**
+     * @Assert\NotBlank()
      * @Assert\Uuid()
      */
     public string $author = '';
--- api/tests/Functional/V1/Task/CreateTest.php
+++ api/tests/Functional/V1/Task/CreateTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Test\Functional\V1\Task;
 
+use Test\Functional\Json;
 use Test\Functional\WebTestCase;
 
 class CreateTest extends WebTestCase
@@ -34,5 +35,40 @@ class CreateTest extends WebTestCase
 
         self::assertEquals(201, $response->getStatusCode(), (string)$response->getBody());
         self::assertEquals('', (string)$response->getBody());
+    }
+
+    public function testEmpty(): void
+    {
+        $response = $this->app()->handle(self::json('POST', '/v1/task/create', []));
+
+        self::assertEquals(422, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
+
+        self::assertEquals([
+            'errors' => [
+                'author' => 'This value should not be blank.',
+                'name' => 'This value is too short. It should have 3 characters or more.'
+            ]
+        ], Json::decode($body));
+    }
+
+    public function testNotExisting(): void
+    {
+        $response = $this->app()->handle(self::json('POST', '/v1/task/create', [
+            'author' => 'not-uuid',
+            'name' => '',
+        ])->withHeader('Accept-Language', 'es;q=0.9, ru;q=0.8, *;q=0.5'));
+
+        self::assertEquals(422, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
+
+        $data = Json::decode($body);
+
+        self::assertEquals([
+            'errors' => [
+                'author' => 'Значение не соответствует формату UUID.',
+                'name' => 'Значение слишком короткое. Должно быть равно 3 символам или больше.'
+            ],
+        ], $data);
     }
 }

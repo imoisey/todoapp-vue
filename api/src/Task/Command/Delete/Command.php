ecb408bdfb08a16ddcd08bf11f4a55e50fba2195
Igor Moiseev <developer.modx@gmail.com>
2020-12-13 00:06:29 +0300

    Added usecase Task\Delete

---
+++ api/src/Task/Command/Delete/Command.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Task\Command\Delete;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Uuid()
+     */
+    public string $id = '';
+}
---
+++ api/src/Task/Command/Delete/Handler.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Task\Command\Delete;
+
+use App\Flusher;
+use App\Task\Entity\Task\Id;
+use App\Task\Entity\Task\TaskRepository;
+
+class Handler
+{
+    private TaskRepository $tasks;
+    private Flusher $flusher;
+
+    public function __construct(TaskRepository $tasks, Flusher $flusher)
+    {
+        $this->tasks = $tasks;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command): void
+    {
+        $task = $this->tasks->get(new Id($command->id));
+
+        $task->delete();
+
+        $this->flusher->flush();
+    }
+}

39af4322dd47ec8056632ecdd8f48052742e975e
Igor Moiseev <developer.modx@gmail.com>
2020-12-20 18:21:05 +0300

    Added event TaskWasExecuted

---
+++ api/src/Task/Entity/Task/Event/TaskWasExecuted.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Task\Entity\Task\Event;
+
+use App\Event\Event;
+use App\Task\Entity\Task\Id;
+
+class TaskWasExecuted implements Event
+{
+    public Id $id;
+
+    public function __construct(Id $id)
+    {
+        $this->id = $id;
+    }
+}
